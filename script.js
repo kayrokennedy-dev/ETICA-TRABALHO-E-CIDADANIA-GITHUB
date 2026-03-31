0ba4327a531ea93bf3101da23d423711ed12aa22
kayrokennedy-dev <kennedykayro74@gmail.com>
2026-03-31 12:39:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var btnRedirecionar = document.getElementById("btn");
 btnRedirecionar.onclick = function() {
     var resposta = confirm("Você será redirecionado para outra página. Deseja continuar?");
     if (resposta) {
-        window.location.href = "https://www.google.com";
+        window.location.href = "https://kayrokennedy-dev.github.io/ETICA-TRABALHO-E-CIDADANIA-GITHUB/video.html";
     } 
 }
